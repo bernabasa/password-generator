6b1d03aedb272effbce487d2454e28bfa4022ed2
bernabas <bernabas.abdeta@yahoo.com>
2022-06-07 12:11:29 -0400

    building password length

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# password-generator
+# Password Generator
+A simple and easy to use password generator that runs in the browser. Generates a randomized password from a series of parameters set by the user.
+
+About
+Set your password's length between 8-128 characters
+
+Verify if you'd like your generated password to include characters such as:
+
+Special Characters (all symbols included are supported across all major opertating systems)
+Integers (numbers)
+Upper-case Characters
+Lower-case Characters
+Project Submission Info
+Created with HTML, CSS, and JavaScript
+Initial HTML and CSS starter code provided by GW Coding Bootcamp
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,45 @@
 // Assignment code here
+var uppercaseChar;
+var lowercaseChar;
+var num;
+var specialChar;
 
+var createStructure = function(){
 
-// Get references to the #generate element
+  var characters ="";
+
+  uppercaseChar = window.confirm("Are you sure you want to access uppercase characters?");
+  if(uppercaseChar) {
+      characters += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  }
+  
+  lowercaseChar = window.confirm("Are you sure you want to access lowercase characters?");
+  if(lowercaseChar) {
+      characters += "abcdefghijklmnopqrstuvwxyz";
+  }
+
+  num = window.confirm("Are you sure you want to accsess the numbers?");
+  if(num) {
+    characters += "0123456789";
+  }
+  specialChar = window.confirm("Are you sure you would like to use the special characters?");
+  if(specialChar) {
+    characters += "#$&()!\"*+,-./:;<=>?@[\\]^_{|}~"
+  }
+
+  return characters;
+}
+
+//THEN I choose a length of at least 8 characters and no more than 128 characters//
+var generatePassword = function() {
+    var passwordLength = window.prompt("please choose the length of your password?");
+
+    while(passwordLength > 128 || passwordLength < 8 isNaN(passwordLength)) {
+      window.alert("The length of the password has to be from 8 to 128.");
+      passwordLength = window.prompt("How many characters do you want it to be?");
+       
+    }
+}
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
